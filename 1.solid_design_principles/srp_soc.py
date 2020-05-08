a80c83b55164820170b61dc46f1512db675ae3a1
ajayraikar-upwork <iit2017040@iiita.ac.in>
2020-05-08 14:49:57 +0530

    added single responsibility principle design pattern python code.

---
+++ 1.solid_design_principles/srp_soc.py
@@ -0,0 +1,48 @@
+# SRP (Single Responsiblity Principle) or SOC (Seperation of Concerns)
+
+
+class Journal:
+    def __init__(self):
+        self.enteries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.count += 1
+        self.enteries.append(f'{self.count}: {text}')
+
+    def remove_entry(self, pos):
+        del self.enteries[pos]
+
+    def __str__(self):
+        return '\n'.join(self.enteries)
+
+    # def save(self, filename):
+    #     file = open(filename, 'w')
+    #     file.write(str(self))
+    #     file.close()
+    #
+    # def load(self, filename):
+    #     pass
+    #
+    # def low_from_web(self, uri):
+    #     pass
+
+class PersistanceManager:
+
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
+
+j = Journal()
+j.add_entry('I read a book today, Neuromencer')
+j.add_entry('I read a blog today')
+print(f'Journal entries:\n{j}')
+
+
+file = r'C:\temp\journal.txt'
+PersistanceManager.save_to_file(j, file)
+
+with open(file) as fh:
+    print(fh.read())
